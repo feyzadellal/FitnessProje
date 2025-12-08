67e30d2df25ed46daec6620e796858d7aaf928a7
feyzadellal <feyzadellall@gmail.com>
2025-12-08 20:34:04 +0300

    Hizmetler modülü eklendi: Controller ve listeleme sayfası (Index) tamamlandı.

---
+++ FitnessApp/FitnessApp/Controllers/HizmetController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FitnessApp.Data;
+using FitnessApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessApp.Controllers
+{
+    public class HizmetController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HizmetController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Hizmetleri getirirken hangi salona ait olduğunu da getir (İlişkisel Veri)
+            var hizmetler = await _context.Hizmetler.Include(h => h.Salon).ToListAsync();
+            return View(hizmetler);
+        }
+    }
+}
