3f26bee79d1d3399ac6fa5969529f6bca6bd8559
feyzadellal <feyzadellall@gmail.com>
2025-12-08 20:24:52 +0300

    Eğitmenler modülü eklendi: Controller ve listeleme sayfası (Index) oluşturuldu.

---
+++ FitnessApp/FitnessApp/Controllers/EgitmenController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FitnessApp.Data;
+using FitnessApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessApp.Controllers
+{
+    public class EgitmenController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EgitmenController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Eğitmenleri Listeleme (Salon bilgisiyle beraber)
+        public async Task<IActionResult> Index()
+        {
+            // Include(e => e.Salon) komutu çok önemli!
+            // Bu komut, "Eğitmeni getirirken çalıştığı Salonun adını da getir" demektir.
+            // İlişkisel veritabanı (Foreign Key) işte burada işe yarar.
+            var egitmenler = await _context.Egitmenler.Include(e => e.Salon).ToListAsync();
+            return View(egitmenler);
+        }
+    }
+}
